a855b1811236fc6efe6e42c6d9f2b5da8adfc35b
IT25102131 <IT25102131@my.sliit.lk>
2026-05-05 14:12:23 +0530

    Update SupplierRepository

--- src/main/java/com/inventory/repository/SupplierRepository.java
+++ src/main/java/com/inventory/repository/SupplierRepository.java
@@ -2,7 +2,7 @@ package src.main.java.com.inventory.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import supplier.backend.model.Supplier;
+import src.main.java.com.inventory.model.Supplier;
 
 @Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
